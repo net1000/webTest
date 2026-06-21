9c4337b04d7609f23ce9a6d1141b059f6a4ad8c3
net1000 <sash.lukanin@yandex.ru>
2026-06-21 21:19:01 +0300

    vite

--- camp/vite.config.js
+++ camp/vite.config.js
@@ -1,18 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
-
+// camp/vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
+  server: {
+    host: '0.0.0.0', // Разрешает прослушивание всех интерфейсов
+    port: 5173,
+    // 👇 ДОБАВЬТЕ ЭТУ СТРОКУ
+    allowedHosts: ['summer-camp-1.ru'], 
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+        secure: false
+      },
+      '/uploads': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
